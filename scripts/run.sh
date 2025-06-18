69e6069c06c85f22f3d9c4e649017935377c8402
caston1981 <40859915+caston1981@users.noreply.github.com>
2025-06-18 14:17:09 +0800

    Update run.sh fixed path

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 echo "Run script is running..."
-# You can add real app startup commands here later
 echo "✅ Flask Middleware container is running!"
 echo "Starting Flask app..."
-exec python app.py
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
-
+exec python web/app.py
