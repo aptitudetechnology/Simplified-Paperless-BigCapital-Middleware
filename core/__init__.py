3ad4e3534167a56cf0407808638246703057d403
caston1981 <40859915+caston1981@users.noreply.github.com>
2025-06-21 17:05:36 +0800

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -4,5 +4,6 @@ Core modules for simplified-paperless-bigcapital-middleware
 """
 
 from .ocr_processor import OCRProcessor, extract_text_from_file
+from .text_extractor import InvoiceDataExtractor, extract_invoice_data
 
-__all__ = ['OCRProcessor', 'extract_text_from_file']
+__all__ = ['OCRProcessor', 'extract_text_from_file', 'InvoiceDataExtractor', 'extract_invoice_data']
